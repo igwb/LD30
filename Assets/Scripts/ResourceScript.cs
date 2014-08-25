41411bb6f55f2fdefa6d5756b882e38b64b19068
igwb <bodobab@googlemail.com>
2014-08-25 19:25:16 +0200

    only spin resources when connected

--- Assets/Scripts/ResourceScript.cs
+++ Assets/Scripts/ResourceScript.cs
@@ -4,15 +4,16 @@ using System.Collections;
 public class ResourceScript : MonoBehaviour {
 	
 	public float resourceAmount;
-	
+	public float resourceDrainSpeed;
+
+
 	public float rotationSpeed;
 	
-	public float drainSpeed;
-	
-	
 	public bool connected;
+
 	// Use this for initialization
 	void Start () {
+
 		rotationSpeed = Random.Range(15.0f,22.0f);
 		
 		if(Random.Range(0,1) > 0.5) {
@@ -23,8 +24,12 @@ public class ResourceScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	 
-	 if(connected) {
-	 	float drainAmount = Time.deltaTime * drainSpeed;
+		 if(!connected) {
+			return;
+		 }
+
+	 	float drainAmount = Time.deltaTime * resourceDrainSpeed;
+
 		if(resourceAmount >=  drainAmount) {
 			HUD.getHUD().bottomPanel.waterValue +=  drainAmount;
 			resourceAmount -=  drainAmount;
@@ -32,13 +37,12 @@ public class ResourceScript : MonoBehaviour {
 			HUD.getHUD().bottomPanel.waterValue += drainAmount;
 			Destroy(gameObject);
 		}
-		
-		}
+	
 		this.transform.RotateAround(transform.position, Vector3.forward,rotationSpeed * Time.deltaTime); 
-		//this.transform.Rotate(0.0f, 0.0f, rotationSpeed * Time.deltaTime);
 	}
 	
 	public void Connect() {
+		
 		connected = true;
 	}
 }
